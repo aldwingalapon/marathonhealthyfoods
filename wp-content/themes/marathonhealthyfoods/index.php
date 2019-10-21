3d8e16ee0c66218fa647aa011c6654692e70480b
Aldwin Galapon <aldwin.galapon@gmail.com>
2019-10-21 15:14:21 +0800

    Marathon Healthy Foods landing caption update 151410212019

--- wp-content/themes/marathonhealthyfoods/index.php
+++ wp-content/themes/marathonhealthyfoods/index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+		<section class="landing_caption"><h2 class="center text-white"><span class="block">We're cooking up something new.</span> <span class="block">Come back soon.</span></h2></section>
+		
 		<div id="main_content">
 			<div class="container">
 				<div class="row">
